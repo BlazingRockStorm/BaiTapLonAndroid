842b57f163c833c89f3ae41dead0491dd5b51dc7
quangtv <quangtv@ominext.com>
2017-05-11 15:11:37 +0700

    Update code: update loading data

--- MyApplication3/app/src/main/java/com/quan/myapplication/Activities/ChapterDetailActivity.java
+++ MyApplication3/app/src/main/java/com/quan/myapplication/Activities/ChapterDetailActivity.java
@@ -1,9 +1,12 @@
 package com.quan.myapplication.Activities;
 
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
 import android.widget.TextView;
 
 import com.quan.myapplication.Utils.Rule;
@@ -11,13 +14,13 @@ import com.quan.myapplication.Entities.Chapter;
 import com.quan.myapplication.Managers.BookManager;
 import com.quan.myapplication.R;
 
-import org.apache.commons.lang3.StringUtils;
-
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class ChapterDetailActivity extends AppCompatActivity {
-    BookManager bookManager;
+    private BookManager bookManager;
+    private TextView textView;
+    private TextView countMisspellings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,34 +37,50 @@ public class ChapterDetailActivity extends AppCompatActivity {
 
         chapter = bookManager.getChapter(chapter.getChapterID());
 
-        Rule rule = new Rule();
-        StringBuilder result = new StringBuilder("");
-        StringTokenizer scanner = new StringTokenizer(chapter.getChapterContent().replaceAll("<", " <").replaceAll(">", "> "));
-        int count = 0;
-        // read word by word
-        while (scanner.hasMoreTokens()) {
-            String word = scanner.nextToken();
+        LoadingTextTask task = new LoadingTextTask();
+        task.execute(chapter.getChapterContent().replaceAll("<", " <").replaceAll(">", "> "));
 
-            System.out.println(" " + word);
+        countMisspellings = (TextView) findViewById(R.id.countMisspellings);
+        textView = (TextView) findViewById(R.id.detailChapter);
 
-            if (word.contains("<") || word.contains(">")) {
-                result.append(word);
-                continue;
-            } else if (rule.checkValid(word) == false) {
-                word = "<font color=#cc0029>" + word + "</font>";
-                count++;
+    }
+
+    private class LoadingTextTask extends AsyncTask<String, Void, Spanned> {
+
+        private int count;
+
+        @Override
+        protected Spanned doInBackground(String... params) {
+            Rule rule = new Rule();
+            StringBuilder result = new StringBuilder("");
+            StringTokenizer scanner = new StringTokenizer(params[0]);
+            count = 0;
+            // read word by word
+            while (scanner.hasMoreTokens()) {
+                String word = scanner.nextToken();
+
+                if (word.contains("<") || word.contains(">")) {
+                    result.append(word);
+                    continue;
+                } else if (rule.checkValid(word) == false) {
+                    word = "<font color=#cc0029>" + word + "</font>";
+                    count++;
+                }
+                result.append(word + " ");
             }
-            result.append(word + " ");
-
-
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                return Html.fromHtml(result.toString(), Html.FROM_HTML_MODE_COMPACT);
+            } else {
+                return Html.fromHtml(result.toString());
+            }
         }
-        TextView countMisspellings = (TextView) findViewById(R.id.countMisspellings);
-        countMisspellings.setText("Có tổng cộng " + count + " lỗi từ vựng trong chương này");
-        TextView textView = (TextView) findViewById(R.id.detailChapter);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            textView.setText(Html.fromHtml(result.toString(), Html.FROM_HTML_MODE_COMPACT));
-        } else {
-            textView.setText(Html.fromHtml(result.toString()));
+
+        @Override
+        protected void onPostExecute(Spanned result) {
+            textView.setText(result);
+            countMisspellings.setText("Có tổng cộng " + count + " lỗi từ vựng trong chương này");
+            findViewById(R.id.detailWrapper).setVisibility(View.VISIBLE);
+            findViewById(R.id.loadingPanel).setVisibility(View.GONE);
         }
     }
 }
