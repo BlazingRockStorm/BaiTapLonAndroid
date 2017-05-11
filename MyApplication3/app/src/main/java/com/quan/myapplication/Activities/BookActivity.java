30a7b30f6c2c80ea2882f13d2367be29226766b4
quangtv <quangtv@ominext.com>
2017-05-11 10:27:07 +0700

    Update code: finish Voice search and use SharePref

--- MyApplication3/app/src/main/java/com/quan/myapplication/Activities/BookActivity.java
+++ MyApplication3/app/src/main/java/com/quan/myapplication/Activities/BookActivity.java
@@ -1,6 +1,10 @@
 package com.quan.myapplication.Activities;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,11 +26,14 @@ import com.quan.myapplication.R;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class BookActivity extends AppCompatActivity {
     private ListView lstBooks;
     private BookManager bookManager;
     private BookAdapter bookAdapter;
+    private EditText searchBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,13 +50,12 @@ public class BookActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setCustomView(R.layout.menu_search);
 
-        final EditText searchBox = (EditText) actionBar.getCustomView().findViewById(
+        searchBox = (EditText) actionBar.getCustomView().findViewById(
                 R.id.searchfield);
         // Cài đặt chế độ hiển thị
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM
                 | ActionBar.DISPLAY_SHOW_HOME);
 
-        ImageButton imgVoiceSearch = (ImageButton) actionBar.getCustomView().findViewById(R.id.search_voice_btn);
 
         searchBox.addTextChangedListener(new TextWatcher() {
             @Override
@@ -66,12 +72,19 @@ public class BookActivity extends AppCompatActivity {
             }
         });
 
+        ImageButton imgVoiceSearch = (ImageButton) actionBar.getCustomView().findViewById(R.id.search_voice_btn);
+
+        imgVoiceSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnToOpenMic();
+            }
+        });
     }
 
-    private void initLstBook(ArrayList<Book> lstBook)
-    {
+    private void initLstBook(ArrayList<Book> lstBook) {
         lstBooks = (ListView) findViewById(R.id.lstBook);
-        bookAdapter  = new BookAdapter(BookActivity.this, R.layout.book_row,lstBook);
+        bookAdapter = new BookAdapter(BookActivity.this, R.layout.book_row, lstBook);
 
         lstBooks.setAdapter(bookAdapter);
         lstBooks.setTextFilterEnabled(true);
@@ -81,10 +94,40 @@ public class BookActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Book book = (Book) bookAdapter.getItem(position);
                 Intent intent = new Intent(BookActivity.this, ChapterActivity.class);
-                intent.putExtra("book", book);
+                SharedPreferences sharedPref = getSharedPreferences(getString(R.string.share_pref),Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt(getString(R.string.book_id), book.getBookID());
+                editor.commit();
                 startActivity(intent);
             }
         });
+    }
+
+    public void btnToOpenMic() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hãy nói tên truyện");
+
+        try {
+            startActivityForResult(intent, 143);
+        } catch (ActivityNotFoundException tim) {
+
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 143:
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> voiceInText = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    searchBox.setText(voiceInText.get(0));
+                }
+                break;
+        }
     }
 
 }
